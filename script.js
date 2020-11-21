ba54c810894b94f029f0835de1420a163978498f
Laura Micheller Peterson <michellepeterson.mishmish@gmail.com>
2020-11-21 12:51:22 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//Ask a series of questions
+var questions = [
+    { q: 'The sky is blue.', a: 't' },
+    { q: 'There are 365 days in a year.', a: 't' },
+    { q: 'There are 42 ounces in a pound.', a: 'f' },
+    { q: 'The Declaration of Independence was created in 1745.', a: 'f' },
+    { q: 'Bananas are vegetables.', a: 'f' }
+];
