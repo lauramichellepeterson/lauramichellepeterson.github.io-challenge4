54680f1bcebe86ba11491a50c3058b2f4474fa01
Laura Micheller Peterson <michellepeterson.mishmish@gmail.com>
2020-11-22 00:52:19 -0700

    add js buttons, remove html buttons

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var mainTextEl = document.querySelector("#main-text");
 var gradingTextEl = document.querySelector("#grading-text");
 var scoreTextEl = document.querySelector("#score-text");
-var startButton = document.querySelector("#start-button");
-var trueButton = document.querySelector("#true-button");
-var falseButton = document.querySelector("#false-button");
+
+// var buttonSection = document.querySelector("#buttons");
+var jsButtons = document.querySelector("#js-buttons");
+var startButtonId = "js-start-button";
+var trueButtonId = "js-true-button";
+var falseButtonId = "js-false-button";
+// var htmlButtons = document.querySelector("#html-buttons");
+// var startButton = document.querySelector("#start-button");
+// var trueButton = document.querySelector("#true-button");
+// var falseButton = document.querySelector("#false-button");
 var score = 0;
 var answer = "";
 var i=0;
@@ -17,9 +24,6 @@ var questions = [
     { q: "Variables do not allow you to save data. ", a: 'f' }
 ];
 
-mainTextEl.textContent = "Start the quiz"
-// createStartButton();
-
 //Start game with score of 0
 // var startQuiz = function() {
 //     score = 0;
@@ -31,9 +35,59 @@ mainTextEl.textContent = "Start the quiz"
 
 var updateQuestionText = function() {
     mainTextEl.textContent = (i+1) + ". " + questions[i].q;
-    // console.log(mainTextEl.textContent);
 };
 
+// var createStartButton = function() {
+//     // create button
+//     var buttonEl = document.createElement("button");
+//     buttonEl.textContent = "JS Start Button";
+//     buttonEl.className = "btn-js-btn";
+//     buttonEl.id = "js-start-button";
+
+//     // add button to DOM
+//     // buttonSection.appendChild(buttonEl);
+//     jsButtons.appendChild(buttonEl);
+// };
+
+// var createTrueButton = function() {
+//     // create button
+//     var buttonEl = document.createElement("button");
+//     buttonEl.textContent = "JS True";
+//     buttonEl.className = "btn-js-btn";
+//     buttonEl.id = "js-true-button";
+
+//     // add button to DOM
+//     // buttonSection.appendChild(buttonEl);
+//     jsButtons.appendChild(buttonEl);
+// };
+
+// var createFalseButton = function() {
+//     // create button
+//     var buttonEl = document.createElement("button");
+//     buttonEl.textContent = "JS False";
+//     buttonEl.className = "btn-js-btn";
+//     buttonEl.id = "js-false-button";
+
+//     // add button to DOM
+//     // buttonSection.appendChild(buttonEl);
+//     jsButtons.appendChild(buttonEl);
+// };
+
+var createButton = function(text,id) {
+    // create button
+    var buttonEl = document.createElement("button");
+    buttonEl.textContent = text;
+    buttonEl.className = "btn-js-btn";
+    buttonEl.id = id;
+
+    // add button to DOM
+    jsButtons.appendChild(buttonEl);
+};
+
+var deleteButton = function(buttonId) {
+    var buttonSelected = document.querySelector(".btn-js-btn[id='" + buttonId + "']");
+    buttonSelected.remove();    
+};
 
 var quizLoop = function() {
     //Compare answers
@@ -52,46 +106,39 @@ var quizLoop = function() {
         i++;
     }
 
-    if (i>=questions.length) {
-        //End the game
-        scoreTextEl.textContent = ("You got " + score + "/" + (questions.length));
-        // console.log("End of Quiz");
-    } else {
+    if (i<questions.length) {
+        // Continue the quiz
         updateQuestionText();
+    } else {
+        // End the quiz
+        scoreTextEl.textContent = ("You got " + score + "/" + (questions.length));
     }
 };
-
-// var createStartButton = function() {
-//     // create list item
-//     var listItemEl = document.createElement("li");
-//     listItemEl.className = "start-button";
-
-//     // add task id as a custom attribute
-//     listItemEl.setAttribute("start-button-id", taskIdCounter);
-
-//     buttonList.appendChild(listItemEl);
-// };
 
 var questionButtonHandler = function(event) {
     //get target element from event
     var targetEl = event.target;
 
+    // if (targetEl.matches(".btn-js-btn[id='js-start-button']") && (i===0) ) {
+    if (targetEl.matches(".btn-js-btn[id='" + startButtonId + "']") && (i===0) ) {    
+            // deleteButton("js-start-button");
+            deleteButton(startButtonId);
+            // createTrueButton();
+            // createFalseButton();
+            createButton("True",trueButtonId);
+            createButton("False",falseButtonId);
+            updateQuestionText();
+            answer = "";
+    } 
     //true button was clicked
-    if (targetEl.matches("#true-button")) {
+    // else if (targetEl.matches(".btn-js-btn[id='js-true-button']")) {
+    else if (targetEl.matches(".btn-js-btn[id='" + trueButtonId + "']")) {
         answer = true;
     }
     //false button was clicked
-    else if (targetEl.matches("#false-button")) {
+    // else if (targetEl.matches(".btn-js-btn[id='js-false-button']")) {
+    else if (targetEl.matches(".btn-js-btn[id='" + falseButtonId + "']")) {
         answer = false;
-    }
-    // start button was clicked
-    else if (targetEl.matches("#start-button")) {
-        if (i===0) {
-            // delete start button then updateQuestionText
-            
-            updateQuestionText();
-        }
-        answer = "";
     }
 
     if (i<questions.length) {
@@ -99,14 +146,16 @@ var questionButtonHandler = function(event) {
     }
 };
 
+mainTextEl.textContent = "Start the quiz"
 
+if (i===0) {
+    // createStartButton();
+    createButton("START",startButtonId);
+} 
 
-//Show total at end
-// alert("You got " + score + "/" + (questions.length-1));
-// scoreTextEl.textContent = ("You got " + score + "/" + (questions.length));
-// console.log(scoreTextEl.textContent); 
-
-
-startButton.addEventListener("click", questionButtonHandler);
-trueButton.addEventListener("click", questionButtonHandler);
-falseButton.addEventListener("click", questionButtonHandler);
+// buttonSection.addEventListener("click", questionButtonHandler);
+// startButton.addEventListener("click", questionButtonHandler);
+// trueButton.addEventListener("click", questionButtonHandler);
+// falseButton.addEventListener("click", questionButtonHandler);
+jsButtons.addEventListener("click", questionButtonHandler);
+// htmlButtons.addEventListener("click", questionButtonHandler);
